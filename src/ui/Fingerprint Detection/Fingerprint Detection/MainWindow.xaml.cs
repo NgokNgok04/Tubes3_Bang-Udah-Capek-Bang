be27ea2f6229793df1fbd1a7de55c6d851d1f6f0
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-05-24 14:08:45 +0700

    feat: connect database

---
+++ src/ui/Fingerprint Detection/Fingerprint Detection/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xml.Serialization;
+
+namespace Fingerprint_Detection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImportSQLFile();
+        }
+
+        public void ImportSQLFile()
+        {
+            DB_init dB_Init = new DB_init();
+            string sqlFilePath = @"..\..\..\..\..\database\tubes3_stima.sql";
+            dB_Init.ExecuteSqlFile(sqlFilePath);
+        }
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+
+                ButtonImage.Source = bitmap;
+                ButtonImage.Width = 240;
+                ButtonImage.Height = 320;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOutput.Visibility = Visibility.Visible;
+            ButtonInput.Visibility = Visibility.Visible;
+            ButtonUpload.Visibility = Visibility.Hidden;
+            SubTitle.Visibility = Visibility.Hidden;
+            TitleInput.Visibility = Visibility.Visible;
+            TitleOutput.Visibility = Visibility.Visible;
+        }
+
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
