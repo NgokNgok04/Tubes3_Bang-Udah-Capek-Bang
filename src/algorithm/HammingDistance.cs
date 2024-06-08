007a2bb1f480ac16f24aba5aecdcfc2ddfb07ae3
Agil Fadillah Sabri <agilfadillahsabri03@gmail.com>
2024-06-08 12:47:06 +0700

    refactor: matriks [,] ti [][]

--- src/algorithm/HammingDistance.cs
+++ src/algorithm/HammingDistance.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections.Generic;
 
 class HammingDistance {
-    public static float CalculateHammingDistance(char[,] str1, char[,] str2) {
+    public static float CalculateHammingDistance(char[][] str1, char[][] str2) {
         // function to calculate hamming distance between two matrices of characters
-        int numberOfCharacters = str1.GetLength(0) * str1.GetLength(1);
+        int numberOfCharacters = str1.Length * str1.Length;
         float distance = 0;
-        for (int i = 0; i < str1.GetLength(0); i++) {
-            for (int j = 0; j < str1.GetLength(1); j++) {
-                if (str1[i, j] != str2[i, j]) {
+        for (int i = 0; i < str1.Length; i++) {
+            for (int j = 0; j < str1.Length; j++) {
+                if (str1[i][j] != str2[i][j]) {
                     distance += 1;
                 }
             }
